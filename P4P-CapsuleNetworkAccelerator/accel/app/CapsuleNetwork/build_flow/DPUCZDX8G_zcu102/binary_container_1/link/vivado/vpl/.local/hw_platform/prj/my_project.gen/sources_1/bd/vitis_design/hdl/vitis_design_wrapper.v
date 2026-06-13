02356198f84a97d93d82cf96f4d5fbc840fb7677
Josiah Sterling <jste910@aucklanduni.ac.nz>
2026-06-13 14:32:09 +1200

    AUTO #12

---
+++ P4P-CapsuleNetworkAccelerator/accel/app/CapsuleNetwork/build_flow/DPUCZDX8G_zcu102/binary_container_1/link/vivado/vpl/.local/hw_platform/prj/my_project.gen/sources_1/bd/vitis_design/hdl/vitis_design_wrapper.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.1 (lin64) Build 5076996 Wed May 22 18:36:09 MDT 2024
+//Date        : Tue May  5 15:10:37 2026
+//Host        : beta-HP-ProBook-430-G6 running 64-bit Ubuntu 20.04.6 LTS
+//Command     : generate_target vitis_design_wrapper.bd
+//Design      : vitis_design_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module vitis_design_wrapper
+   ();
+
+
+  vitis_design vitis_design_i
+       ();
+endmodule
